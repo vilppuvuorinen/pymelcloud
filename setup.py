f38f7dcf8eb21b3b1aaa451d7513eeb40fe032dd
Vilppu Vuorinen <vilppu.vuorinen@jubic.fi>
2019-12-26 11:12:37 +0200

    Bump version to 0.1.1
    A CHANGELOG was also added.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="pymelcloud",
-    version="0.1.0",
+    version="0.1.1",
     description="Python MELCloud interface",
     author="Vilppu Vuorinen",
     author_email="vilppu.jotain@gmail.com",
