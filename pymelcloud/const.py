a4fdec281f28ca2b8475ba6016d51ffff0ada175
Espen Fjellvær Olsen <espen@mrfjo.org>
2021-05-11 21:45:49 +0200

    Prevent forbidden request to `ListDeviceUnits` on guest devices
    Calling ListDeviceUnits on devices where you are not the owner, but are
    granted control will return a 403 Forbidden error.

--- pymelcloud/const.py
+++ pymelcloud/const.py
@@ -4,6 +4,11 @@ DEVICE_TYPE_ATA = "ata"
 DEVICE_TYPE_ATW = "atw"
 DEVICE_TYPE_UNKNOWN = "unknown"
 
+ACCESS_LEVEL = {
+    "GUEST": 3,
+    "OWNER": 4,
+}
+
 DEVICE_TYPE_LOOKUP = {
     0: DEVICE_TYPE_ATA,
     1: DEVICE_TYPE_ATW,
